4d5c03bc013e2791686f2ac2e95b0c65f53dc504
ayatakesi <ayanokoji.takesi@gmail.com>
2018-01-07 22:13:41 +0900

    add dictinary.el settings

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;;; gettext
 (setq auto-mode-alist
       (cons '("\\.po\\'\\|\\.po\\." . po-mode) auto-mode-alist))
@@ -14,5 +25,42 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'lisp-interaction-mode 'eldoc-mode)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;dict-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'dictionary-search "dictionary" 
+	  "Ask for a word and search it in all dictionaries" t)
+(autoload 'dictionary-match-words "dictionary"
+	  "Ask for a word and search all matching words in the dictionaries" t)
+(autoload 'dictionary-lookup-definition "dictionary" 
+	  "Unconditionally lookup the word at point." t)
+(autoload 'dictionary "dictionary"
+	  "Create a new dictionary buffer" t)
+(autoload 'dictionary-mouse-popup-matching-words "dictionary"
+	  "Display entries matching the word at the cursor" t)
+(autoload 'dictionary-popup-matching-words "dictionary"
+	  "Display entries matching the word at the point" t)
+(autoload 'dictionary-tooltip-mode "dictionary"
+	  "Display tooltips for the current word" t)
+(unless (boundp 'running-xemacs)
+	(autoload 'global-dictionary-tooltip-mode "dictionary"
+		  "Enable/disable dictionary-tooltip-mode for all buffers" t))
+;; key bindings for dictionary.el
+(global-set-key "\C-cs" 'dictionary-search)
+(global-set-key "\C-cm" 'dictionary-match-words)
+
 ; else
 (global-set-key (kbd "C-t") 'other-window)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dictionary-server "localhost")
+ '(package-selected-packages (quote (link connection dictionary magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
