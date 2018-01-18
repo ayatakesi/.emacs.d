bb72d9a6477d0a1313f6c8acc7660c486c981467
ayatakesi <ayanokoji.takesi@gmail.com>
2018-01-19 07:31:01 +0900

    for migration

---
+++ elpa/link-1.10/link-autoloads.el
@@ -0,0 +1,15 @@
+;;; link-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("link.el") (23120 55342 890995 411000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; link-autoloads.el ends here
