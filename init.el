dea653651b8eb01405a612ed4f9e81c3f79a4dcb
ayatakesi <ayanokoji.takesi@gmail.com>
2020-01-15 22:19:15 +0900

    2020-01-15

--- init.el
+++ init.el
@@ -10,12 +10,8 @@
 
 ;; theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-# or
-(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'vscode-default-dark t)
-
+;(load-theme 'vscode-default-dark t)
+(load-theme 'zenburn t)
 
 ;; CUI
 (xterm-mouse-mode 1)
@@ -29,27 +25,23 @@
 
 ;; global settings
 (delete-selection-mode)
-<<<<<<< HEAD
 
-;; mbsync + mu
-;; http://pragmaticemacs.com/emacs/migrating-from-offlineimap-to-mbsync-for-mu4e/
+;; ;; mbsync + mu
+;; ;; http://pragmaticemacs.com/emacs/migrating-from-offlineimap-to-mbsync-for-mu4e/
 
-;; path追加
-(add-to-list 'load-path "/data/data/com.termux/files/usr/share/emacs/site-lisp/mu/mu4e")
-(require 'mu4e)
+;; ;; path追加
+;; (add-to-list 'load-path "/data/data/com.termux/files/usr/share/emacs/site-lisp/mu/mu4e")
+;; (require 'mu4e)
 
-;;location of my maildir
-(setq mu4e-maildir (expand-file-name "~/.mail"))
+;; ;;location of my maildir
+;; (setq mu4e-maildir (expand-file-name "~/.mail"))
 
-;;command used to get mail
-(setq mu4e-get-mail-command "mbsync gmail")
+;; ;;command used to get mail
+;; (setq mu4e-get-mail-command "mbsync gmail")
 
-;;rename files when moving
-;;NEEDED FOR MBSYNC
-(setq mu4e-change-filenames-when-moving t)
-
-=======
->>>>>>> f93e69f796c7d734cb9714020a4ea866156ee13b
+;; ;;rename files when moving
+;; ;;NEEDED FOR MBSYNC
+;; (setq mu4e-change-filenames-when-moving t)
 
 ;; Show/hide Emacs dired details in style
 ;; http://xenodium.com/showhide-emacs-dired-details-in-style/
@@ -163,6 +155,8 @@
 ; else
 (global-set-key (kbd "C-t") 'other-window)
 (global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "C-c C-l") 'redraw-display)
+(global-set-key (kbd "C-c C-z") 'bury-buffer)
 
 (let ((my-next-whitespace-minus1 (lambda ()
 				   (interactive)
@@ -179,17 +173,10 @@
 			    (kill-region
 			     (region-beginning)
 			     (region-end))
-			    (goto-char curr))))
-      
-      (my-termux-dialog (lambda ()
-			  (interactive)
-			  (let ((ans (shell-command-to-string "termux-dialog")))
-			    (string-match "^  \"text\": \"\\(.*\\)\"$" ans)
-			    (insert (match-string 1 ans))))))
+			    (goto-char curr)))))
   
   (global-set-key (kbd "C-c C-SPC") my-next-whitespace-minus1)
-  (global-set-key (kbd "C-c C-f") my-forward-sexp2)
-  (global-set-key (kbd "C-c C-z") my-termux-dialog))
+  (global-set-key (kbd "C-c C-f") my-forward-sexp2))
 
 ;;; ddskk
 (when (require 'skk nil t)
@@ -204,24 +191,37 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Info-default-directory-list '("/data/data/com.termux/files/usr/share/info/"))
- '(browse-url-browser-function 'eww-browse-url)
+ '(Info-default-directory-list (quote ("/data/data/com.termux/files/usr/share/info/")))
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(browse-url-browser-function (quote eww-browse-url))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(compilation-scroll-output t)
  '(custom-safe-themes
-   '("f3455b91943e9664af7998cc2c458cfc17e674b6443891f519266e5b3c51799d" default))
+   (quote
+    ("2ebf3d81b7555f8c8cfc42017b00dc47f1fbe80ffce53439c9401da3989fedcd" "90b33f03c0e30ffcc66c8227fb69ec27644f497b68c07fbca5eaa751e3c4caa7" "f3455b91943e9664af7998cc2c458cfc17e674b6443891f519266e5b3c51799d" default)))
  '(dictionary-server "localhost")
  '(elfeed-feeds
-   '("http://planet.gnu.org/rss20.xml" "http://static.fsf.org/fsforg/rss/blogs.xml" "https://static.fsf.org/fsforg/rss/news.xml" "http://sachachua.com/blog/feed/"))
- '(gnus-select-method '(nntp "news.gmane.org"))
+   (quote
+    ("http://planet.gnu.org/rss20.xml" "http://static.fsf.org/fsforg/rss/blogs.xml" "https://static.fsf.org/fsforg/rss/news.xml" "http://sachachua.com/blog/feed/")))
+ '(fci-rule-color "#383838")
+ '(gnus-select-method (quote (nntp "news.gmane.org")))
  '(google-translate-default-source-language "en")
  '(google-translate-default-target-language "ja")
  '(makeinfo-options "--fill-column=56")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-archives
-   '(("melpa" . "http://melpa.org/packages/")
+   (quote
+    (("melpa" . "http://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/")))
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
  '(package-selected-packages
-   '(po-mode zenburn-theme request markdown-mode markdown-mode+ markdown-preview-mode gh-md flymd el2markdown anzu auto-complete diredfl use-package elpa-clone ivy-posframe eldoc-box crux neotree ag elisp-demos helpful ddskk minimap htmlize google-translate pdf-tools pandoc package-utils elfeed link connection magit))
+   (quote
+    (po-mode zenburn-theme request markdown-mode markdown-mode+ markdown-preview-mode gh-md flymd el2markdown anzu auto-complete diredfl use-package elpa-clone ivy-posframe eldoc-box crux neotree ag elisp-demos helpful ddskk minimap htmlize google-translate pdf-tools pandoc package-utils elfeed link connection magit)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(po-default-file-header
    "# SOME DESCRIPTIVE TITLE.
 # Copyright (C) YEAR Free Software Foundation, Inc.
@@ -240,10 +240,32 @@ msgstr \"\"
 ")
  '(scroll-margin 2)
  '(skk-cdb-large-jisyo nil)
- '(skk-initial-search-jisyo "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L"))
+ '(skk-initial-search-jisyo "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t (:background "color-18")))))
